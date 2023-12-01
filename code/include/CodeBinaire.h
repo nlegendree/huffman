de8612cead43841b80052c92c646c632d103249e
Bressac Augustin <augustin.bressac@insa-rouen.fr>
2023-12-01 18:24:42 +0000

    Update CodeBinaire.h

--- code/include/CodeBinaire.h
+++ code/include/CodeBinaire.h
@@ -1,13 +1,18 @@
 #ifndef  __CODEBINAIRE__
 #define __CODEBINAIRE__
+
 struct CodeBinaire
 {
-long int nbBits;
-long int bits[];
+    long int nbBits;
+    long int bits[];
 };
 
 CodeBinaire codeBinaire();
+
 void ajouterBit();
+
 long int obtenirLongueur();
+
 Bit obtenirIemeBit();
+
 #endif
