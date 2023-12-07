2ad0ccd024e2540d8da623055670413dca8ff0f1
Legendre Nathan <nathan.legendre@insa-rouen.fr>
2023-12-07 22:41:23 +0100

    préfixe testArbre et modifs FileDePriorité

--- code/src/FileDePriorite.c
+++ code/src/FileDePriorite.c
@@ -1,69 +1,61 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include "Octet.h"
+#include "Arbre.h"
 
 #define FP_MAX_TAILLE = 255
 
-// Définition de la structure ElementFile
+
 typedef struct {
-    Octet octet;
+    ArbreDeHuffman arbre;
     unsigned int priorite;
 } ElementFile;
 
-// Définition de la structure FileDePriorite
+
 typedef struct {
-    ElementFile tab[FP_MAX_TAILLE]; // Remplacer MAX_TAILLE par la taille maximale souhaitée
+    ElementFile tab[FP_MAX_TAILLE];
     unsigned int tailleActuelle;
 } FileDePriorite;
 
-// Fonction pour créer une file de priorité vide
-FileDePriorite FP_fileDePrioriteVide() {
+
+FileDePriorite FP_fileDePriorite() {
     FileDePriorite temp;
     temp.tailleActuelle = 0;
     return temp;
 }
 
-// Procédure pour ajouter un élément à la file de priorité
-void FP_ajouterElement(Octet octet, unsigned int prio, FileDePriorite *pfile) {
-    unsigned int i > 0;
-    pfile->tailleActuelle++;
-    i = pfile->tailleActuelle;
 
-    while (i > 1 && pfile->tab[i - 1].priorite <= prio) {
-        pfile->tab[i] = pfile->tab[i - 1];
+void FP_ajouterElement(ArbreDeHuffman arbre, unsigned int priorite, FileDePriorite *pfile) {
+    *pfile.tailleActuelle++;
+    i = *pfile.tailleActuelle;
+    while (i > 1 && *pfile.tab[i].priorite <= priorite) {
+        *pfile.tab[i] = *pfile.tab[i - 1];
         i--;
     }
-
-    file->tab[i].octet = octet;
-    file->tab[i].priorite = prio;
+    *pfile.tab[i].arbre = arbre;
+    *pfile.tab[i].priorite = priorite;
 }
 
-// Procédure pour supprimer le dernier élément de la file de priorité
-void FP_supprimerDernier(FileDePriorite *file) {
-    // Précondition : La file de priorité n'est pas vide
-    if (!estVide(*file)) {
-        file->tailleActuelle--;
-        file->tab[file->tailleActuelle].priorite = 0;
+
+void FP_supprimerDernier(FileDePriorite *pfile) {
+    if (!estVide(*pfile)) {
+        *pfile.tailleActuelle--;
+        *pfile.tab[pfile.tailleActuelle - 1].priorite = 0;
     }
 }
 
-// Fonction pour obtenir le dernier élément de la file de priorité
-Octet FP_obtenirDernier(FileDePriorite file) {
-    // Précondition : La file de priorité n'est pas vide
+
+ArbreDeHuffman FP_obtenirDernier(FileDePriorite *pfile) {
     if (!estVide(file)) {
-        return file.tab[file.tailleActuelle - 1].octet;
-    } else {
-        // Gérer l'erreur ou renvoyer une valeur par défaut
-        return ELEMENT_PAR_DEFAUT;
+        return pfile.tab[pfile.tailleActuelle - 1];
     }
 }
 
-// Fonction pour vérifier si la file de priorité est vide
-bool FP_estVide(FileDePriorite file) {
-    return (file.tailleActuelle == 0);
+
+bool FP_estVide(FileDePriorite *pfile) {
+    return (pfile.tailleActuelle == 0);
 }
 
-// Fonction pour obtenir la longueur de la file de priorité
-int FP_longueur(FileDePriorite file) {
-    return file.tailleActuelle;
+
+unsigned int FP_longueur(FileDePriorite *pfile) {
+    return pfile.tailleActuelle;
 }
--- code/src/tests/testArbre.c
+++ code/src/tests/testArbre.c
@@ -10,34 +10,34 @@ int clean_suite_success(void) {
   return 0; 
 }
 
-ArbreDeHuffman arbre = arbreDeHuffman('A', 5);
-ArbreDeHuffman arbreGauche = arbreDeHuffman('B', 3);
-ArbreDeHuffman arbreDroit = arbreDeHuffman('C', 2);
-ArbreDeHuffman combinaison = combiner(arbreGauche, arbreDroit);
+ArbreDeHuffman arbre = AH_arbreDeHuffman('A', 5);
+ArbreDeHuffman arbreGauche = AH_arbreDeHuffman('B', 3);
+ArbreDeHuffman arbreDroit = AH_arbreDeHuffman('C', 2);
+ArbreDeHuffman combinaison = AH_combiner(arbreGauche, arbreDroit);
 
-void test_obtenirElement(void) {
-    CU_ASSERT_TRUE(obtenirElement(arbre)=='A');
+void test_AH_obtenirElement(void) {
+    CU_ASSERT_TRUE(AH_obtenirElement(arbre)=='A');
 }
 
-void test_combiner(void) {
-    CU_ASSERT_TRUE(obtenirPondération(combinaison) == (obtenirPondération(arbreGauche) + obtenirPondération(arbreDroit)));
+void test_AH_combiner(void) {
+    CU_ASSERT_TRUE(AH_obtenirPondération(combinaison) == (AH_obtenirPondération(arbreGauche) + AH_obtenirPondération(arbreDroit)));
 }
 
-void test_obtenirPondération(void) {
-    CU_ASSERT_TRUE(obtenirPondération(arbre), 5);
+void test_AH_obtenirPondération(void) {
+    CU_ASSERT_TRUE(AH_obtenirPondération(arbre), 5);
 }
 
-void test_estUneFeuille(void) {
-    CU_ASSERT_TRUE(estUneFeuille(arbre));
-    CU_ASSERT_FALSE(estUneFeuille(combinaison));
+void test_AH_estUneFeuille(void) {
+    CU_ASSERT_TRUE(AH_estUneFeuille(arbre));
+    CU_ASSERT_FALSE(AH_estUneFeuille(combinaison));
 }
 
-void test_obtenirArbreGauche(void) {
-    CU_ASSERT_TRUE(obtenirArbreGauche(combinaison) == arbreGauche);
+void test_AH_obtenirArbreGauche(void) {
+    CU_ASSERT_TRUE(AH_obtenirArbreGauche(combinaison) == arbreGauche);
 }
 
-void test_obtenirArbreDroit(void) {
-    CU_ASSERT_TRUE(obtenirArbreDroit(combinaison) == arbreDroit);
+void test_AH_obtenirArbreDroit(void) {
+    CU_ASSERT_TRUE(AH_obtenirArbreDroit(combinaison) == arbreDroit);
 }
 
 
@@ -56,12 +56,12 @@ int main(int argc, char** argv){
   }
 
   /* Ajout des tests à la suite de tests boite noire */
-  if ((NULL == CU_add_test(pSuite, "obtenirElement", test_obtenirElement))
-      || (NULL == CU_add_test(pSuite, "combiner", test_combiner))
-      || (NULL == CU_add_test(pSuite, "obtenirPonderation", test_obtenirPonderation))
-      || (NULL == CU_add_test(pSuite, "estUneFeuille", test_estUneFeuille))
-      || (NULL == CU_add_test(pSuite, "obtenirArbreGauche", test_obtenirArbreGauche))
-      || (NULL == CU_add_test(pSuite, "obtenirArbreDroit", test_obtenirArbreDroit))
+  if ((NULL == CU_add_test(pSuite, "AH_obtenirElement", test_AH_obtenirElement))
+      || (NULL == CU_add_test(pSuite, "AH_combiner", test_AH_combiner))
+      || (NULL == CU_add_test(pSuite, "AH_obtenirPonderation", test_AH_obtenirPonderation))
+      || (NULL == CU_add_test(pSuite, "AH_estUneFeuille", test_AH_estUneFeuille))
+      || (NULL == CU_add_test(pSuite, "AH_obtenirArbreGauche", test_AH_obtenirArbreGauche))
+      || (NULL == CU_add_test(pSuite, "AH_obtenirArbreDroit", test_AH_obtenirArbreDroit))
       ) 
     {
       CU_cleanup_registry();
