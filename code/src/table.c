7401aa55c38201590b55c499c03f306014775312
Sentchev Vassili <vassili.sentchev@insa-rouen.fr>
2023-12-19 10:16:32 +0000

    Update table.c

--- code/src/table.c
+++ code/src/table.c
@@ -1,9 +1,10 @@
-#include "table.h"
-#include "octet.h"
-#include "codeBinaire.h"
-#include <assert.h>
 
-TDC_TableDeCodage TDC_tableDeCodage(){
+#include "Table.h"
+#include "octet.h"
+#include <assert.h>
+#include <stdio.h>
+
+/*TDC_TableDeCodage TDC_tableDeCodage(){
     TDC_TableDeCodage tableDeCodage;
     tableDeCodage.nbElements = 0;
 }
@@ -54,5 +55,66 @@ O_Octet TDC_obtenirOctetCode(TDC_TableDeCodage tableDeCodage, CB_CodeBinaire cod
             return tableDeCodage.table[i].octet;
         }
     }
+}*/
+
+TDC_TableDeCodage TDC_tableDeCodage(){
+    TDC_TableDeCodage tableDeCodage;
+    tableDeCodage.nbElements = 0;
+
+    return tableDeCodage;
 }
+
+int TDC_estVide(TDC_TableDeCodage tableDeCodage) {
+    return tableDeCodage.nbElements == 0;
+}
+
+int TDC_octetPresent(TDC_TableDeCodage tableDeCodage, O_Octet o) {
+    
+    for (int i=0; i<tableDeCodage.nbElements; i++) {
+        if (O_egale(tableDeCodage.table[i].octet, o)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+int TDC_codeBinairePresent(TDC_TableDeCodage tableDeCodage, CB_CodeBinaire codeBinaire) {
+    for (int i=0; i<tableDeCodage.nbElements; i++) {
+        if (CB_egale(tableDeCodage.table[i].code, codeBinaire)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+void TDC_ajouterOctet(TDC_TableDeCodage* pTableDeCodage, O_Octet octet, CB_CodeBinaire codeBinaire) {
+    assert(!TDC_octetPresent(*pTableDeCodage, octet));
+    assert(pTableDeCodage->nbElements < 255);
+    pTableDeCodage->nbElements++;
+    pTableDeCodage->table[pTableDeCodage->nbElements-1].octet = octet;
+    pTableDeCodage->table[pTableDeCodage->nbElements-1].code = codeBinaire;
+}
+
+
+
+
+O_Octet TDC_obtenirOctetCode(TDC_TableDeCodage tableDeCodage, CB_CodeBinaire codeBinaire)
+{
+    assert(TDC_codeBinairePresent(tableDeCodage, codeBinaire));
+    for (int i = 0; i < tableDeCodage.nbElements; i++)
+    {
+        if (CB_egale(tableDeCodage.table[i].code, codeBinaire))
+        {
+            return tableDeCodage.table[i].octet;
+        }
+    }
+
+}
+
+
+
 
