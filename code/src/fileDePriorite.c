4e42c56ed4d7d4dcfd3e5360c5cd19b819f8bde0
Legendre Nathan <nathan.legendre@insa-rouen.fr>
2024-01-05 12:40:17 +0100

    modif file

--- code/src/fileDePriorite.c
+++ code/src/fileDePriorite.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <assert.h>
 #include "fileDePriorite.h"
+#include "octet.h"
 
 
 FP_FileDePriorite FP_fileDePriorite() {
     FP_FileDePriorite temp;
+    for (int i = 0; i <= 255; ++i) {
+        temp.arbres[i] = ABR_arbreDeHuffman(O_octet(0), 0);
+    }
     temp.tailleActuelle = 0;
     return temp;
 }
@@ -16,7 +21,9 @@ void FP_ajouterElement(FP_FileDePriorite *pfile, ABR_ArbreDeHuffman arbre) {
     unsigned int priorite = ABR_obtenirPonderation(arbre);
     pfile->tailleActuelle++;
     i = pfile->tailleActuelle;
+    printf("test1");
     while (i > 1 && ABR_obtenirPonderation(&arbre[i]) <= priorite) {
+        printf("test2");
         pfile->arbres[i] = pfile->arbres[i - 1];
         i--;
     }
@@ -26,13 +33,17 @@ void FP_ajouterElement(FP_FileDePriorite *pfile, ABR_ArbreDeHuffman arbre) {
 
 void FP_supprimerDernier(FP_FileDePriorite *pfile) {
     assert(!FP_estVide(*pfile));
-    pfile->tailleActuelle--;
+    if (pfile->tailleActuelle > 0) {
+        pfile->tailleActuelle--;
+    }
 }
 
 
 ABR_ArbreDeHuffman FP_obtenirDernier(FP_FileDePriorite file) {
     assert(!FP_estVide(file));
-    return (file.arbres[file.tailleActuelle]);
+    if (file.tailleActuelle > 0) {
+        return (file.arbres[file.tailleActuelle - 1]);
+    }
 }
 
 
@@ -44,3 +55,4 @@ bool FP_estVide(FP_FileDePriorite file) {
 unsigned int FP_longueur(FP_FileDePriorite file) {
     return file.tailleActuelle;
 }
+
