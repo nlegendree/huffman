dd0da02700ed747b92af70bc8c37c876aaee8a2e
Janneau Lou-Anne <lou-anne.janneau@insa-rouen.fr>
2024-01-07 18:02:38 +0000

    Update file stats.h

--- code/include/stats.h
+++ code/include/stats.h
@@ -1,3 +1,12 @@
+/**
+ * \file stats.h
+ * \brief Implantation du TAD Statistiques
+ * \author L. Janneau
+ * \version 1.0
+ * \date 19/12/2023
+ *
+ */
+
 #ifndef  __STATS__
 #define __STATS__
 
@@ -5,6 +14,10 @@
 
 #define MAX 0x100
 
+/**
+ * \brief Le type ST_Statistiques permet de représenter des statistiques d'apparition d'octets
+ *
+ */
 typedef struct ST_Statistiques
 {
     long int nbOccurenceTotal;
@@ -12,6 +25,7 @@ typedef struct ST_Statistiques
 } ST_Statistiques;
 
 /**
+ * @fn ST_Statistiques ST_statistiques(void)
  * @brief Permet de créer des statistiques vides
  * 
  * @return ST_Statistiques
@@ -19,6 +33,7 @@ typedef struct ST_Statistiques
 ST_Statistiques ST_statistiques(void);
 
 /**
+ * @fn long int ST_obtenirOccurenceOctet(ST_Statistiques statistiques,O_Octet octet)
  * @brief Permet d'obtenir le nombre d'occurences d'un octet
  * 
  * @param statistiques 
@@ -28,6 +43,7 @@ ST_Statistiques ST_statistiques(void);
 long int ST_obtenirOccurenceOctet(ST_Statistiques statistiques,O_Octet octet);
 
 /**
+ * @fn void ST_incrementerOccurenceOctet(O_Octet octet, ST_Statistiques* pstatistiques)
  * @brief Permet d'incrémenter le nombre d'occurences d'un octet
  * 
  * @param octet
@@ -36,6 +52,7 @@ long int ST_obtenirOccurenceOctet(ST_Statistiques statistiques,O_Octet octet);
 void ST_incrementerOccurenceOctet(O_Octet octet, ST_Statistiques* pstatistiques);
 
 /**
+ * @fn void ST_fixerOccurenceOctet(O_Octet octet, long int nbOccurence, ST_Statistiques* pstatistiques)
  * @brief Permet de fixer le nombre d'occurences d'un octet
  * 
  * @param octet
@@ -45,6 +62,7 @@ void ST_incrementerOccurenceOctet(O_Octet octet, ST_Statistiques* pstatistiques)
 void ST_fixerOccurenceOctet(O_Octet octet, long int nbOccurence, ST_Statistiques* pstatistiques);
 
 /**
+ * @fn long int ST_obtenirTotalOccurence(ST_Statistiques statistiques)
  * @brief Permet d'obtenir le nombre total d'occurences (tout octet compris)
  * 
  * @param statistiques
